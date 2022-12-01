3dbf86e1ceec3b99d0c3d6fdedef872ff82abdf0
Dan Kiuna <dkkiuna11@gmail.com>
2022-12-01 17:02:42 -0600

    part 2

--- day1/index.js
+++ day1/index.js
@@ -1,12 +1,11 @@
-// Part 1
-
 var fs = require('fs');
 
 var inputData = fs.readFileSync('input.txt').toString().split("\n");
 var values = []
 var currentSum = 0;
 
-for(i in inputData) {
+//Part 1
+for ( i in inputData) {
     if(inputData[i].length) {
         currentSum = currentSum + Number(inputData[i])
     } else {
@@ -14,5 +13,13 @@ for(i in inputData) {
         currentSum = 0;
     }
 }
-
 console.log(values.sort((a, b) => {return b - a}))
+
+//Part 2
+var sorted = values.sort((a, b) => {return b - a})
+var top3 = 0
+
+for ( i in sorted.slice(0,3)){
+    top3 += sorted.slice(0,3)[i]
+}
+console.log(top3)
